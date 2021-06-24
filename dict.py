90ae15f0b9b13d195db225cc9a224e3185c652c8
ARYAN <aryanbhatia111@gmail.com>
2021-06-24 08:51:44 +0530

    Smart Word checker

--- dict.py
+++ dict.py
@@ -1,17 +1,23 @@
 import json
+import difflib
+from difflib import SequenceMatcher
+from difflib import get_close_matches
+
 data = json.load(open("data.json"))
-conti = "y"
 
-def check(conti):
-    while conti == "y":
-        word = input("Enter a word: ")
-        print("\n", data[word], "\n")
-        conti = input("Do you want to continue(y or n): ")
-        if conti == "n":
-            break
-        print("\n")
+def matcher(word):
+    word = word.lower()
+    if word in data.keys():
+        return data[word]
+    elif len(get_close_matches(word, data.keys(), n = 1, cutoff = 0.8)) > 0:
+        print("did you mean %s instead? " % get_close_matches(word, data.keys(), n = 1, cutoff = 0.8)[0])
+        check = input("y or n? ")
+        if  check != "y":
+            return("enter word again")
+        else:
+            return data[get_close_matches(word, data.keys(), n = 1, cutoff = 0.8)[0]]
+    else:
+        return "word doesn't exists"
 
-try:
-    check(conti)
-except:
-    print("\n", "invalid Try a different word", "\n")
+word = input("Enter a word: ")
+print(matcher(word))
